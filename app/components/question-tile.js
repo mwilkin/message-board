31425658ac040ecede7db4890d722682f5bbbc8e
Matt Wilkin <mwilkin22@gmail.com>
2016-08-26 16:42:36 -0700

    service fixed.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
 
   answerCount: Ember.computed('question.answers', function(){
     var answerCount = this.get('question.answers.length');
