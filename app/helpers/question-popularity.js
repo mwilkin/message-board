92a24be589270a4d852497e307465bd42c9dbd06
Matt Wilkin <mwilkin22@gmail.com>
2016-08-26 08:13:07 -0700

    add helper to mark popular quesitons and questions without answers.

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -3,8 +3,12 @@ import Ember from 'ember';
 export function questionPopularity(params/*, hash*/) {
   var question = params[0];
 
-  if(question.get('answers').get('length') <1 ) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>' + '<span class="glyphicon glyphicon-fire"></span>')
+  if(question.get('answers').get('length') == 0 ) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-flag"></span>');
+  } else if (question.get('answers').get('length') < 3 ){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  } else if (question.get('answers').get('length') > 3 ){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>' + '<span class="glyphicon glyphicon-fire"></span>');    
   }
 }
 
