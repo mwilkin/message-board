32c23b47486c01b4150689e10539d588629e6f50
Matt Wilkin <mwilkin22@gmail.com>
2016-08-28 22:13:48 -0700

    add code for removing and clear favorite question list; neither is working.

--- app/components/display-questions.js
+++ app/components/display-questions.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteQuestions: Ember.inject.service(),
+
+  actions: {
+    remove(question) {
+      this.get('favoriteQuestions').remove(question);
+    },
+    empty() {
+      this.get('favoriteQuestions').clear();
+    }
+  }
 });
